ac35ebd49a55fa900dc25703f6ebadcd32062975
kobyon <77970971+kobyon@users.noreply.github.com>
2025-11-06 16:39:17 +0100

    feat: add variables to since and unitl in git log

--- timeforing.sh
+++ timeforing.sh
@@ -2,11 +2,14 @@
 
 source="${HOME}/.local/src/upheads"
 report="${HOME}/.local/src/git-report-$(date -d yesterday +%Y-%m-%d).txt"
+committer_email="oliver.sakkestad@upheads.no"
+since="yesterday 00:00"
+until="yesterday 23:59"
 
 for d in ${source} ; do
   if [ -d "$d/.git" ]; then
     echo "=== $d ===" >> "${report}"
-    (cd "$d" && git log --since="yesterday 00:00" --until="yesterday 23:59" --committer="oliver.sakkestad@upheads.no" --pretty=format:"%s" >> "${Report}")
+    (cd "$d" && git log --since="${since}" --until="${until}" --committer="${committer_email}" --pretty=format:"%s" >> "${Report}")
     echo >> "${report}"
   fi
 done
