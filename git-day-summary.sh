de4601f25793208979145dc312d89697639a8237
kobyon <77970971+kobyon@users.noreply.github.com>
2025-11-06 16:53:58 +0100

    refactor: change the script to return today, instead yesterday

--- git-day-summary.sh
+++ git-day-summary.sh
@@ -3,8 +3,8 @@
 source="${HOME}/.local/src/upheads/puppet"
 report="./git-report-$(date -d yesterday +%Y-%m-%d).txt"
 committer_email="oliver.sakkestad@upheads.no"
-since="yesterday 00:00"
-until="yesterday 23:59"
+since="today 00:00"
+until="today 23:59"
 
 for d in "${source}"/* ; do
   if [ -d "$d/.git" ]; then
