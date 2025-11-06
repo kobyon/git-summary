dfadcc6f56c946f509937c72dc46216825638e2f
kobyon <77970971+kobyon@users.noreply.github.com>
2025-11-06 16:57:01 +0100

    refactor: make git log command easier to read

--- git-day-summary.sh
+++ git-day-summary.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-source="${HOME}/.local/src/upheads/puppet"
+source="${HOME}/.local/src/upheads/"
 report="./git-report-$(date -d yesterday +%Y-%m-%d).txt"
 committer_email="oliver.sakkestad@upheads.no"
 since="today 00:00"
@@ -9,7 +9,13 @@ until="today 23:59"
 for d in "${source}"/* ; do
   if [ -d "$d/.git" ]; then
     echo "=== $d ===" >> "${report}"
-    git --no-pager --git-dir="$d/.git" --work-tree="$d" log --since="${since}" --until="${until}" --author="${committer_email}" --pretty=format:"%s"
+
+    git --no-pager --git-dir="$d/.git" --work-tree="$d" log \
+    --since="${since}" \
+    --until="${until}" \
+    --author="${committer_email}" \
+    --pretty=format:"%s"
+
     echo >> "${report}"
   fi
 done
