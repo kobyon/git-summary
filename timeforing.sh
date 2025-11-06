f16be71f70414409342069bdfa82289b70f050e3
kobyon <77970971+kobyon@users.noreply.github.com>
2025-11-06 16:39:40 +0100

    fix: wrong variable name in git log command

--- timeforing.sh
+++ timeforing.sh
@@ -9,7 +9,7 @@ until="yesterday 23:59"
 for d in ${source} ; do
   if [ -d "$d/.git" ]; then
     echo "=== $d ===" >> "${report}"
-    (cd "$d" && git log --since="${since}" --until="${until}" --committer="${committer_email}" --pretty=format:"%s" >> "${Report}")
+    (cd "$d" && git log --since="${since}" --until="${until}" --committer="${committer_email}" --pretty=format:"%s" >> "${report}")
     echo >> "${report}"
   fi
 done
