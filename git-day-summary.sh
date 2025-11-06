ac0397c652c892f1793f74ddc8d1c30dd098b617
kobyon <77970971+kobyon@users.noreply.github.com>
2025-11-06 16:52:21 +0100

    fix: error in lopping of subfolders

--- git-day-summary.sh
+++ git-day-summary.sh
@@ -6,7 +6,7 @@ committer_email="oliver.sakkestad@upheads.no"
 since="yesterday 00:00"
 until="yesterday 23:59"
 
-for d in ${source} ; do
+for d in "${source}"/* ; do
   if [ -d "$d/.git" ]; then
     echo "=== $d ===" >> "${report}"
     (cd "$d" && git log --since="${since}" --until="${until}" --committer="${committer_email}" --pretty=format:"%s" >> "${report}")
