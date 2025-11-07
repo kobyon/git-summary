a2f13bc7bf71f1e1ef7b98dab16e3fbd929d83e6
kobyon <77970971+kobyon@users.noreply.github.com>
2025-11-07 12:57:31 +0100

    feat(create_report): only create log entries on repos that have history

--- git-day-summary.sh
+++ git-day-summary.sh
@@ -9,21 +9,26 @@ report_folder="${HOME}/reports"
 committer_emails="oliver.sakkestad@upheads.no 77970971+kobyon@users.noreply.github.com"
 generate_history=${1:-3} # How many days of history?
 
+git_log() {
+  printf '%s' "${committer_emails}" | awk '{ for( i=1; i<=NF; i++) print $i }' | while read -r email; do
+    git --no-pager --git-dir="${3}/.git" --work-tree="${3}" log --all \
+    --since="${1}" \
+    --until="${2}" \
+    --author="${email}" \
+    --pretty=format:"%ad %s" --date=format:'%H:%M'
+  done
+}
+
 create_report() {
   # Variable reference
   # 1 - since
   # 2 - until
-
   find "${source}" -type d -name ".git" -exec dirname {} \+ | while read -r d; do
-  printf "=== %s ===\n" "${d#"${source}"}"
-
-    printf '%s' "${committer_emails}" | awk '{ for( i=1; i<=NF; i++) print $i }' | while read -r email; do
-      git --no-pager --git-dir="${d}/.git" --work-tree="${d}" log --all \
-      --since="${1}" \
-      --until="${2}" \
-      --author="${email}" \
-      --pretty=format:"%ad %s" --date=format:'%H:%M'
-    done
+  _git_log=$(git_log "${1}" "${2}" "${d}")
+    if [ -n "${_git_log}" ]; then
+      printf "=== %s ===\n" "${d#"${source}"}"
+      printf "%s\n" "${_git_log}"
+    fi
   done
 }
 
