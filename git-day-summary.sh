970fd87538dfa63634366af3def0795188785acd
kobyon <77970971+kobyon@users.noreply.github.com>
2025-11-07 13:58:00 +0100

    docs: add documentation to functions and key parts of code

--- git-day-summary.sh
+++ git-day-summary.sh
@@ -10,6 +10,10 @@ committer_emails="oliver.sakkestad@upheads.no 77970971+kobyon@users.noreply.gith
 generate_history=${1:-3} # How many days of history?
 
 git_log() {
+  # Variable reference
+  # 1 - since
+  # 2 - until
+  # 3 - git directory
   printf '%s' "${committer_emails}" | awk '{ for( i=1; i<=NF; i++) print $i }' | while read -r email; do
     git --no-pager --git-dir="${3}/.git" --work-tree="${3}" log --all \
     --since="${1}" \
@@ -24,17 +28,19 @@ create_report() {
   # 1 - since
   # 2 - until
   find "${source}" -type d -name ".git" -exec dirname {} \+ | while read -r d; do
-    _git_log=$(git_log "${1}" "${2}" "${d}")
+    _git_log=$(git_log "${1}" "${2}" "${d}") # Store the value of git_log in a variable so we can run checks
     if [ -n "${_git_log}" ]; then
       project=$(printf "%s" "${d#"${source}"}" | awk -F'/' '{print $2}')
-      printf "=== %s ===\n" "${project}"
+      printf "=== %s ===\n" "${project}" # Print a project header
       printf "%s\n" "${_git_log}"
     fi
   done
 }
 
-mkdir -p "${report_folder}"
+mkdir -p "${report_folder}" # Make sure the project folder exist
 
+# Main loop
+# Looping trough all days you want to generate reports for
 for i in $(seq 0 $((generate_history - 1))); do
   day=$(date -d "${i} days ago" +%Y-%m-%d)
   since="${day} 07:00"
