9bf55530e6b06a1ecb2072579487fa8015c4f6c5
kobyon <77970971+kobyon@users.noreply.github.com>
2025-11-07 09:26:43 +0100

    feat: create report for the last 7 days

--- git-day-summary.sh
+++ git-day-summary.sh
@@ -1,23 +1,27 @@
 #!/bin/sh
 
 source="${HOME}/.local/src/"
-report="./git-report-$(date -d yesterday +%Y-%m-%d).txt"
 committer_email="oliver.sakkestad@upheads.no"
-since="yesterday 00:00"
-until="yesterday 23:59"
 
-find "${source}" -type d -name ".git" -exec dirname {} \+ | while read -r d; do
-  if [ -d "${d}/.git" ]; then
-    {
-      printf "%s\n" "=== ${d} ==="
+for i in $(seq 0 6); do
+  day=$(date -d "$i days ago" +%Y-%m-%d)
+  since="$day 00:00"
+  until="$day 23:59"
+  report="./git-report-$day.txt"
 
-      git --no-pager --git-dir="${d}/.git" --work-tree="${d}" log --all \
-      --since="${since}" \
-      --until="${until}" \
-      --author="${committer_email}" \
-      --pretty=format:"%s"
+  find "${source}" -type d -name ".git" -exec dirname {} \+ | while read -r d; do
+    if [ -d "${d}/.git" ]; then
+      {
+        printf "%s\n" "=== ${d} ==="
 
-      printf "\n"
-    } >> "${report}"
-  fi
+        git --no-pager --git-dir="${d}/.git" --work-tree="${d}" log --all \
+        --since="${since}" \
+        --until="${until}" \
+        --author="${committer_email}" \
+        --pretty=format:"%s"
+
+        printf "\n"
+      } >> "${report}"
+    fi
+  done
 done
