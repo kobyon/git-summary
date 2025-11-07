953925d6b02fd9faba6c2572c94d0c392ea0c0f3
kobyon <77970971+kobyon@users.noreply.github.com>
2025-11-07 09:52:07 +0100

    refactor: move report creation group to a function
    Making the code simpler and easier to call on the function when nessesary

--- git-day-summary.sh
+++ git-day-summary.sh
@@ -3,25 +3,27 @@
 source="${HOME}/.local/src/"
 committer_email="oliver.sakkestad@upheads.no"
 
+create_report() {
+  find "${source}" -type d -name ".git" -exec dirname {} \+ | while read -r d; do
+    if [ -d "${d}/.git" ]; then
+      printf "%s\n" "=== ${d} ==="
+
+      git --no-pager --git-dir="${d}/.git" --work-tree="${d}" log --all \
+      --since="${1}" \
+      --until="${2}" \
+      --author="${committer_email}" \
+      --pretty=format:"%s"
+
+      printf "\n"
+    fi
+  done
+}
+
 for i in $(seq 0 6); do
   day=$(date -d "$i days ago" +%Y-%m-%d)
   since="$day 00:00"
   until="$day 23:59"
   report="./git-report-$day.txt"
 
-  find "${source}" -type d -name ".git" -exec dirname {} \+ | while read -r d; do
-    if [ -d "${d}/.git" ]; then
-      {
-        printf "%s\n" "=== ${d} ==="
-
-        git --no-pager --git-dir="${d}/.git" --work-tree="${d}" log --all \
-        --since="${since}" \
-        --until="${until}" \
-        --author="${committer_email}" \
-        --pretty=format:"%s"
-
-        printf "\n"
-      } > "${report}"
-    fi
-  done
+  create_report "${since}" "${until}" > "${report}"
 done
