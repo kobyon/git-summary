eae5881f041ffa078746a0dd9e8929d53b4d3ae1
kobyon <77970971+kobyon@users.noreply.github.com>
2025-11-07 09:23:54 +0100

    feat: search all refs in repo

--- git-day-summary.sh
+++ git-day-summary.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 
 source="${HOME}/.local/src/"
-report="./git-report-$(date -d today +%Y-%m-%d).txt"
+report="./git-report-$(date -d yesterday +%Y-%m-%d).txt"
 committer_email="oliver.sakkestad@upheads.no"
-since="today 00:00"
-until="today 23:59"
+since="yesterday 00:00"
+until="yesterday 23:59"
 
 find "${source}" -type d -name ".git" -exec dirname {} \+ | while read -r d; do
   if [ -d "${d}/.git" ]; then
     {
       printf "%s\n" "=== ${d} ==="
 
-      git --no-pager --git-dir="${d}/.git" --work-tree="${d}" log \
+      git --no-pager --git-dir="${d}/.git" --work-tree="${d}" log --all \
       --since="${since}" \
       --until="${until}" \
       --author="${committer_email}" \
